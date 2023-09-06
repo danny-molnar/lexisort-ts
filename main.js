88086affe5e8bab000c9156126f931548742325c
Danny Molnar <danny.molnar@cloudreach.com>
2023-09-06 10:42:59 +0100

    feat: added sort logic

---
+++ main.js
@@ -0,0 +1,7 @@
+function sortStrings(strings) {
+    return strings.sort();
+}
+// Example usage:
+var strings = ["banana", "apple", "cherry"];
+var sortedStrings = sortStrings(strings);
+console.log(sortedStrings); // ["apple", "banana", "cherry"]
---
+++ main.ts
@@ -0,0 +1,8 @@
+function sortStrings(strings: string[]): string[] {
+    return strings.sort();
+}
+
+// Example usage:
+let strings = ["banana", "apple", "cherry"];
+let sortedStrings = sortStrings(strings);
+console.log(sortedStrings); // ["apple", "banana", "cherry"]
